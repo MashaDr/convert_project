4ae040be3c50cd4359f8462506772407b66b3795
masha.dr <masha.dr@gmail.com>
2022-01-07 09:01:17 -0700

    [*] Changed settings.py::ALLOWED_HOSTS.

--- api/Num2Words.py
+++ api/Num2Words.py
@@ -53,7 +53,7 @@ class Num2Words:
         result = list()
         for i in range(len(Num2Words.bigs)):
             # Splits a number into 3-digit chunks starting from the right side
-            # For example, num=1345678. It processes 678 first, 345 next, and 1 at the end. 
+            # For example, num=1345678. It processes 678 first, 345 next, and 1 at the end.
             if num % 1000 != 0:
                 tmp = [Num2Words.bigs[i]] + result if i != 0 else result
                 result = Num2Words.to_words(num % 1000) + tmp
