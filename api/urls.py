7d85e3628e7fe371374962329eea6a0d82e25334
masha.dr <masha.dr@gmail.com>
2022-01-07 11:11:39 -0700

    [+] Added view.py::num_to_english1.

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('num_to_english', views.num_to_english, name='convert'),
+    path('num_to_english1', views.num_to_english1, name='convert1'),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from .Num2Words import Num2Words
 import json
-
 
 def handler_not_found(request, exception=None):
     return JsonResponse({
@@ -20,7 +20,8 @@ def handler_server_error(request):
 #@ensure_csrf_cookie
 def num_to_english(request):
     try:
-        if request.method  not in ("POST", "GET"):
+        print(f'request.method  ==> {request.method }')
+        if request.method not in ("POST", "GET"):
             return JsonResponse({"status": 'error',
                                  "num_in_english": f'{request.method} is not allowed'
                                  })
@@ -44,3 +45,23 @@ def num_to_english(request):
                              })
 
 
+@api_view(['GET', 'POST'])
+def num_to_english1(request):
+    try:
+        data = json.loads(request.body) if request.method == "POST" else request.GET
+        number = data.get('number')
+
+        if number and number.isnumeric():
+            # convert a number to the english words
+            words = Num2Words.convert(int(number))
+        else:
+            words = 'Error: Invalid input.'
+
+        code = "error" if 'Error' in words else "ok"
+        return JsonResponse({"status": code,
+                             "num_in_english": words
+                             })
+    except Exception as e:
+        return JsonResponse({"status": 'error',
+                             "num_in_english": 'Internal error'
+                             })
