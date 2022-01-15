b00e42f7bbbb828d8a0303576af6723212a6553b
masha.dr <masha.dr@gmail.com>
2022-01-15 05:37:01 -0700

    [+] Added num2words.

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework.response import Response
 from .Num2Words import Num2Words
+from typing import Union
 import json
 
 
@@ -17,35 +18,22 @@ def handler_server_error(request):
     return JsonResponse({"status": 500,
                          "error": "Unfortunately we're having trouble loading the page you are looking for."})
 
-
 @csrf_exempt
+# @ensure_csrf_cookie
 def num_to_english(request):
     try:
         if request.method not in ("POST", "GET"):
-            return JsonResponse({"status": 'error',
-                                 "num_in_english": f'{request.method} is not allowed'
-                                 })
+            raise Exception(f'{request.method} is not allowed')
+
         data = json.loads(request.body) if request.method == "POST" else request.GET
         number = data.get('number')
-
-        if number and number.isnumeric():
-            # convert a number to the english words
-            words = Num2Words.convert(int(number))
-        else:
-            words = 'Error: Invalid input.'
-
-        code = "error" if 'Error' in words else "ok"
-        return JsonResponse({"status": code,
-                             "num_in_english": words
-                             })
+        return JsonResponse(num2words(number))
     except json.decoder.JSONDecodeError:
         return JsonResponse({"status": 'error',
-                             "num_in_english": 'Incorrect JSON format'
-                             })
+                             "num_in_english": 'Incorrect JSON format'})
     except Exception as e:
         return JsonResponse({"status": 'error',
-                             "num_in_english": f'Internal error {e}'
-                             })
+                             "num_in_english": f'Internal error: {e}'})
 
 
 @api_view(['GET'])
@@ -62,18 +50,24 @@ def num_to_words(request):
     try:
         data = request.data if request.method == "POST" else request.query_params
         number = data.get('number')
-
-        if number and number.isnumeric():
-            # convert a number to the english words
-            words = Num2Words.convert(int(number))
-        else:
-            words = 'Error: Invalid input.'
-
-        code = "error" if 'Error' in words else "ok"
-        return Response({"status": code,
-                         "num_in_english": words
-                         })
+        return Response(num2words(number))
     except Exception as e:
         return Response({"status": 'error',
-                         "num_in_english": f'Internal error {e}'
-                         })
+                         "num_in_english": f'Internal error {e}'})
+
+
+def num2words(number: Union[str, int, None]) -> dict:
+    """
+     A function that convert a non-negative integer num to its English words representation using Num2Words.
+     @param number: An integer.
+     @return: A dictionary { "status": "ok", "num_in_english": "English words"}
+    """
+    # Checking if the 'number' variable is a numeric string or an integer.
+    if number and ((type(number) is str and number.isnumeric()) or type(number) is int):
+        # convert a number to the english words
+        words = Num2Words.convert(int(number))
+    else:
+        words = 'Error: Invalid input.'
+
+    code = "error" if 'Error' in words else "ok"
+    return {"status": code, "num_in_english": words}
